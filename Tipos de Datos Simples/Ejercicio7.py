5eccd5a8b12de11a9cda6dc186058451ae7d5fa3
Alejandro Alejandro <atienzabermudez@gmail.com>
2026-09-23 14:10:30 +0200

    Ejercicio7

---
+++ Tipos de Datos Simples/Ejercicio7.py
@@ -0,0 +1,4 @@
+peso = float(input("Introduce tu peso en kg: "))
+estatura = float(input("Introduce tu estatura en metros: "))
+imc = peso / (estatura ** 2)
+print(f"Tu índice de masa corporal es: {imc:.2f}")
