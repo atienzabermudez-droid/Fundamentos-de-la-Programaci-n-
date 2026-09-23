dce08b6cf5ce11d6ba7838b1da66206e86122ceb
Alejandro Alejandro <atienzabermudez@gmail.com>
2026-09-23 14:19:28 +0200

    Ejercicio8

---
+++ Tipos de Datos Simples/Ejercicio8.py
@@ -0,0 +1,7 @@
+n = int(input("Introduce el primer número entero: "))
+m = int(input("Introduce el segundo número entero: "))
+
+c = n // m
+r = n % m
+
+print(f"{n} entre {m} da un cociente {c} y un resto {r}")
