57fc0f03601ad6ec9f3ee29ff8036d5ee97e8fd7
Alejandro Alejandro <atienzabermudez@gmail.com>
2026-09-25 14:39:26 +0200

    Ejercicio12

---
+++ Tipos de Datos Simples/Ejercicio12.py
@@ -0,0 +1,6 @@
+BarrasdePanNuevas=3.49
+BarrasdePanViejas= BarrasdePanNuevas * 0.6
+print(f"El precio de las barras de pan nuevas es: {BarrasdePanNuevas}€")
+print(f"El descuento aplicado es del 60%")
+print(f"El precio de las barras de pan viejas es: {BarrasdePanViejas:.2f}€")
+
